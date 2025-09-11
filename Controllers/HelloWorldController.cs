3d8407f64b62eb41a12c442415c512a4d1995d11
deni5-c <denisbobeica18@gmail.com>
2025-09-11 22:10:24 +0300

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_ecommerce.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello World";
+
+            return View();
+        }
+    }
+}
